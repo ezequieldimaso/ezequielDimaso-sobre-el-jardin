7f7e5b0b7b3256bf8aa56a1411233991976fab84
Ezequiel Dimaso <ezequieldimaso@hotmail.com.ar>
2022-03-10 18:00:55 -0300

    agregando promesas

--- src/components/items/ItemListContainer.jsx
+++ src/components/items/ItemListContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ItemCount from "../items/ItemCount"
 import Cards from "../cards/Cards"
 import "../cards/Cards.css"
@@ -9,6 +9,32 @@ import "../items/Items.css"
 const onAdd = ()=>{
   console.log("agrego productos al carrito")
 }
+
+function imprimirProductos (){
+  return new Promise((resolve ,reject)=>{
+
+    setTimeout(()=>{
+      let productos=[
+        {nombre:"Cintas. Mala madre. Chlorophytum comosum",precio:1200,descripcion: "herbácea perenne"},
+        {nombre:"Cintas. Mala madre. Chlorophytum comosum",precio:1200,descripcion: "herbácea perenne"},
+        {nombre:"Cintas. Mala madre. Chlorophytum comosum",precio:1200,descripcion: "herbácea perenne"}  
+      ]
+      if(productos.length === 0){
+        reject("error arreglo vacio")
+      }else{
+        resolve(productos)
+      }
+    },3000)
+  }
+)}
+
+imprimirProductos()
+.then((productos)=>{
+  console.log(productos)
+})
+.catch((err)=>{
+  console.log(err)
+})
 
 const ItemListContainer = ({mensaje}) => {
   return (
